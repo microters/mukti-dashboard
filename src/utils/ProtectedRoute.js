df653b68d2a803690b948a8e5b82d67e84f49a27
codershawon <shawonb500@gmail.com>
2025-03-18 12:34:07 +0530

    add appointment design and some data fetching completed

--- src/utils/ProtectedRoute.js
+++ src/utils/ProtectedRoute.js
@@ -1,14 +1,21 @@
+import React from "react";
 import { Navigate } from "react-router-dom";
 
+const ProtectedRoute = ({ children, allowedRoles }) => {
+  const token = localStorage.getItem("authToken");
+  const user = JSON.parse(localStorage.getItem("user"));
 
-const ProtectedRoute = ({ children }) => {
-  const token =localStorage.getItem("authToken");
+  if (!token || !user) {
+    return <Navigate to="/login" replace />;
+  }
 
-  if (!token) {
-    return <Navigate to="http://localhost:3000/login" replace />;
+  // Check if the user role is allowed
+  if (allowedRoles && !allowedRoles.includes(user.role)) {
+    return <Navigate to="/dashboard" replace />; // Redirect unauthorized users
   }
 
   return children;
 };
 
 export default ProtectedRoute;
+
